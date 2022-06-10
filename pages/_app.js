3864a6aac956c05593b7a1be56e0619edbc345b8
manuetov <manuetov@hotmail.com>
2022-06-10 18:41:01 +0200

    el barco del arroz

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,30 @@
 import React from 'react';
+import { Layout } from '../components'
 
-import '../styles/globals.scss';
-import { Layout } from '../components';
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
+  )
 }
 
-export default MyApp;
-
+export default MyApp
 
 // import React from 'react';
-// import { Layout } from '../components'
 
-// import 'tailwindcss/tailwind.css'
-// import '../styles/globals.scss'
+// import '../styles/globals.scss';
+// import { Layout } from '../components';
 
 // function MyApp({ Component, pageProps }) {
 //   return (
 //     <Layout>
 //       <Component {...pageProps} />
 //     </Layout>
-//   )
+//   );
 // }
 
-// export default MyApp
+// export default MyApp;
